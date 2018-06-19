73b160c12513d97a5fb10d0c3a48cf21198fd2c6
lexo <lexo@mbp>
2018-06-19 11:50:02 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+#! /usr/bin/env node
+let userArgs = process.argv.slice(2);
+const git = require('simple-git')('.');
+
+let branch = "master"
+let remote = "origin"
+let commit = "commit message"
+if(userArgs.length === 3){
+  remote = userArgs[0]
+  branch = userArgs[1]
+  commit = userArgs[2]
+}
+else if(userArgs.length === 2){
+  remote = userArgs[0]
+  commit = userArgs[1]
+} else if(userArgs.length === 1) {
+  commit = userArgs[0]
+} else {
+  console.log('\x1b[36m%s\x1b[0m',"Formas de Uso:")
+  console.log('\x1b[36m%s\x1b[0m',"- gitpush \"Mensaje Commit\"")
+  console.log('\x1b[36m%s\x1b[0m',"- gitpush [origin] \"Mensaje Commit\"")
+  console.log('\x1b[36m%s\x1b[0m',"- gitpush [origin] [master] \"Mensaje Commit\"")
+  console.log('\x1b[0m',"")
+  process.exit(0);
+}
+git.add('./*')
+git.commit(commit)
+git.pull()
+git.push(['-u', remote, branch], () => console.log('\x1b[36m%s\x1b[0m',`Pushed to ${remote}/${branch}`));
