ace4c9f6f7ebab7b3bc9d58a0e2fba99f51f392e
lexo <lexo@mbp>
2018-06-19 12:02:00 -0600

    first commit

--- index.js
+++ index.js
@@ -5,6 +5,15 @@ const git = require('simple-git')('.');
 let branch = "master"
 let remote = "origin"
 let commit = "commit message"
+
+// set current branch
+git.branchLocal((err, data) => {
+  if (!err) {
+    console.log('\x1b[36m%s\x1b[0m',`Working on branch ${data.current}`)
+    branch = data.current;
+  }
+});
+
 if(userArgs.length === 3){
   remote = userArgs[0]
   branch = userArgs[1]
@@ -19,11 +28,17 @@ else if(userArgs.length === 2){
   console.log('\x1b[36m%s\x1b[0m',"Formas de Uso:")
   console.log('\x1b[36m%s\x1b[0m',"- gitpush \"Mensaje Commit\"")
   console.log('\x1b[36m%s\x1b[0m',"- gitpush [origin] \"Mensaje Commit\"")
-  console.log('\x1b[36m%s\x1b[0m',"- gitpush [origin] [master] \"Mensaje Commit\"")
+  // console.log('\x1b[36m%s\x1b[0m',"- gitpush [origin] [master] \"Mensaje Commit\"")
   console.log('\x1b[0m',"")
   process.exit(0);
 }
-git.add('./*')
+
+git.outputHandler((command, stdout, stderr) => {
+  stdout.pipe(process.stdout);
+  stderr.pipe(process.stderr);
+})
+
+git.add('.')
 git.commit(commit)
 git.pull()
-git.push(['-u', remote, branch], () => console.log('\x1b[36m%s\x1b[0m',`Pushed to ${remote}/${branch}`));
+git.push([remote, branch], () => console.log('\x1b[36m%s\x1b[0m',`Done`));
