dbd8f7774b8bbffef9ebc0a6e208caa8c7f43129
Andrés Alonzo y Alonzo <5524124+aalonzolu@users.noreply.github.com>
2019-06-10 17:20:48 -0600

    Update functions.js

--- functions.js
+++ functions.js
@@ -5,8 +5,7 @@ exports.
     console.log('\x1b[0m',"")
     process.exit(0);
   }
-  git.add('.')
-  git.commit(commit)
+  
   try {
     git.pull(remote,branch ,(err, data) => {
       if(err) {
@@ -17,5 +16,7 @@ exports.
   } catch (e) {
     console.log('\x1b[36m%s\x1b[0m',`New branch`)
   }
+  git.add('.')
+  git.commit(commit)
   git.push([remote, branch], () => console.log('\x1b[36m%s\x1b[0m',`Done`));
 }
