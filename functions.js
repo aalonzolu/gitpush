58d74ca216c4b0851c13cc7a14d91cd17022c1d5
Andrés Alonzo y Alonzo <5524124+aalonzolu@users.noreply.github.com>
2021-09-13 09:58:13 -0600

    Update functions.js
    Fix git pull

--- functions.js
+++ functions.js
@@ -5,18 +5,11 @@ exports.
     console.log('\x1b[0m',"")
     process.exit(0);
   }
-  
-  try {
-    git.pull(remote,branch ,(err, data) => {
-      if(err) {
-        console.log('\x1b[36m%s\x1b[0m',`Error while git pull`)
-//         git.push([remote, branch], () => console.log('\x1b[36m%s\x1b[0m',`Done`));
-      }
-    })
-  } catch (e) {
-    console.log('\x1b[36m%s\x1b[0m',`Error while git pull`)
-    //console.log('\x1b[36m%s\x1b[0m',`New branch`)
-  }
+  git.pull(remote,branch ,(err, data) => {
+    if(err) {
+      console.log('\x1b[36m%s\x1b[0m',`Error while git pull`)
+    }
+  })
   git.add('.')
   git.commit(commit)
   git.push([remote, branch], () => console.log('\x1b[36m%s\x1b[0m',`Done`));
